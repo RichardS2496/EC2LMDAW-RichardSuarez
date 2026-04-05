306f16b932f766f4cb9adf81b16e297009f66517
Richard Suarez <richardmsc24@gmail.com>
2026-04-05 16:26:15 +0200

    btn-back-start added and finished contact section

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-let year = new Date().getFullYear();
+const year = new Date().getFullYear();
 document.getElementById("year").textContent = year;
+
+const btnBackStart = document.getElementById("btn-back-start");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 720) {
+    btnBackStart.classList.add("visible");
+  } else {
+    btnBackStart.classList.remove("vivible");
+  }
+
+  if (window.scrollY < 720) {
+    btnBackStart.classList.remove("visible");
+  }
+});
